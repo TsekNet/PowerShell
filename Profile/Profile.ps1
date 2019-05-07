f93d71add6bf36c901aa5444bde1925c24a8ae65
tseknet <dan@tseknet.com>
2019-05-06 22:37:54 -0400

    Major overhaul of profile for real this time

--- Profile/Profile.ps1
+++ Profile/Profile.ps1
@@ -10,7 +10,14 @@
   To use this profile, simply place this file in any of your $profile
   directories and restart your PowerShell console
   (Ex: $profile.CurrentUserAllHosts)
+
+.LINK
+  TsekNet.com
+  GitHub.com/TsekNet
+  Twitter.com/TsekNet
 #>
+
+#region function declarations
 
 # Helper function to change directory to my development workspace
 function Set-Path {
@@ -21,32 +28,54 @@ function Set-Path {
   Set-Location $Path
 }
 
-# Ensure that required modules are loaded
-try {
-  Import-Module Get-ChildItemColor -ErrorAction Stop
-}
-catch {
-  Install-Module Get-ChildItemColor -Scope CurrentUser -Force
-  Import-Module Get-ChildItemColor
+# Helper function to copy the last command entered
+function Copy-LastCommand {
+  Get-History -id $(((Get-History) | Select-Object -Last 1 |
+      Select-Object ID -ExpandProperty ID)) |
+  Select-Object -ExpandProperty CommandLine |
+  clip
 }
 
-# Set ll and ls alias to use the new Get-ChildItemColor cmdlets
-Set-Alias ll Get-ChildItemColor -Option AllScope
-Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+# Helper function to ensure all modules are loaded, with error handling
+function Get-MyModules {
+  try {
+    Import-Module posh-git -ErrorAction Stop
+  }
+  catch {
+    Install-Module posh-git -Scope CurrentUser -Force
+    Import-Module posh-git
+  }
+
+  try {
+    Import-Module oh-my-posh -ErrorAction Stop
+  }
+  catch {
+    Install-Module oh-my-posh -Scope CurrentUser -Force
+    Import-Module oh-my-posh
+  }
+
+  try {
+    Import-Module Get-ChildItemColor -ErrorAction Stop
+  }
+  catch {
+    Install-Module Get-ChildItemColor -Scope CurrentUser -Force
+    Import-Module Get-ChildItemColor
+  }
+}
 
 # Helper function to test prompt elevation
 function Test-IsAdministrator {
-  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+  if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
+    $n = "Administrator"
+  }
+  else {
+    $n = "Non-Admin"
+  }
 }
 
-if (Test-IsAdministrator) {
-  $n = "Administrator"
-}
-else {
-  $n = "Non-Admin"
-}
-Write-Output "PowerShell [$($n) | $(([regex]"\d+\.\d+.\d+").match($psversiontable.psversion).value) | $($psversiontable.psedition)]"
+#endregion
+
+#region statements
 
 # If it's Windows PowerShell, we can turn on Verbose output if you're holding shift
 if ("Desktop" -eq $PSVersionTable.PSEdition) {
@@ -63,30 +92,28 @@ if ("Desktop" -eq $PSVersionTable.PSEdition) {
   }
 }
 
-# PowerLine Settings
-# Ensure that required modules are loaded
-try {
-  Import-Module posh-git -ErrorAction Stop
-}
-catch {
-  Install-Module posh-git -Scope CurrentUser -Force
-  Import-Module posh-git
-}
+#endregion
 
-# Ensure that required modules are loaded
-try {
-  Import-Module oh-my-posh -ErrorAction Stop
-}
-catch {
-  Install-Module oh-my-posh -Scope CurrentUser -Force
-  Import-Module oh-my-posh
-}
+#region execution
+
+Test-IsAdministrator
+
+$host.ui.RawUI.WindowTitle = "PowerShell [ $($n) |
+$(([regex]"\d+\.\d+.\d+").match($psversiontable.psversion).value) |
+$($psversiontable.psedition) |
+$env:username@$env:COMPUTERNAME.$env:USERDOMAIN ]"
+
+# Import all my modules
+Get-MyModules
+
+# Set ll and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias ll Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 
 # Set the oh-my-posh theme
 Set-Theme Paradox
 
-# Remove username from PowerLine
-Set-Variable -Name DefaultUser -Value $env:USERNAME -Scope Global
-
 # Set the current directory to the one set in the function above
 Set-Path
+
+#endregion
