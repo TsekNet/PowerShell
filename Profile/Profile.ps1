f23e3a9679cfa2571260f23e3302f250686c8ab2
tseknet <dan@tseknet.com>
2019-05-11 10:37:03 -0400

    Add Quotes to profile load

--- Profile/Profile.ps1
+++ Profile/Profile.ps1
@@ -189,6 +189,72 @@ begin {
     End {
     }
   }
+
+  <#
+.Synopsis
+   Get a Quote for any topc
+.DESCRIPTION
+   Get-Quote cmdlet data harvests a/multiple quote(s) from  Web outputs into your powershell console
+.EXAMPLE
+   PS > Quote -Topic "success"
+   For me success was always going to be a Lamborghini. But now I've got it, it just sits on my drive.
+   Curtis Jackson [50 Cent], American Rapper. From his interview with Louis Gannon for Live magazine, The Mail on Sunday (UK) newspaper, (25 October 2009).
+.EXAMPLE
+   PS > "love", "genius"| Quote
+   To be able to say how much you love is to love but little.
+   Petrarch, To Laura in Life (c. 1327-1350), Canzone 37
+   Doing easily what others find it difficult is talent; doing what is impossible for talent is genius.
+   Henri-Frédéric Amiel, Journal
+.EXAMPLE
+   PS > Get-Quote -Topic "Genius" -Count 2
+   No age is shut against great genius.
+   Seneca the Younger, Epistolæ Ad Lucilium, CII
+
+   Genius is a capacity for taking trouble.
+   Leslie Stephen, reported in Bartlett's Familiar Quotations, 10th ed. (1919)
+.INPUTS
+   Inputs to this cmdlet (if any)
+.OUTPUTS
+   Output from this cmdlet (if any)
+.NOTES
+   This cmdlet uses "https://en.wikiquote.org" to pull the information
+.COMPONENT
+   The component this cmdlet belongs to
+.ROLE
+   The role this cmdlet belongs to
+.FUNCTIONALITY
+   The functionality that best describes this cmdlet
+#>
+  function Get-Quote {
+    [CmdletBinding()]
+    [Alias('Quote')]
+    [OutputType([String])]
+    Param(
+      # Topic of the Quote
+      [Parameter(Mandatory, ValueFromPipeline,
+        ValueFromPipelineByPropertyName, Position = 0)]
+      [ValidateNotNullOrEmpty()][String[]]$Topic,
+      [Parameter(Position = 1)][Int]$Count = 1 ,
+      [Parameter(Position = 2)][Int]$Length = 150
+    )
+
+    Process {
+      Foreach ($Item in $Topic) {
+        $URL = "https://en.wikiquote.org/wiki/$Item"
+        Try {
+          $WebRequest = Invoke-WebRequest $URL
+          $WebRequest.ParsedHtml.getElementsByTagName('ul') |
+          Where-Object { $_.parentElement.classname -eq "mw-parser-output" -and $_.innertext.length -lt $Length } |
+          Get-Random -Count $Count |
+          ForEach-Object {
+            Write-Host $_.innertext -ForegroundColor Green
+          }
+        } catch {
+          $_.exception
+        }
+      }
+    }
+  }
 }
 
 #endregion
@@ -219,8 +285,11 @@ end {
   # Admin verification is used for the title window
   Test-IsAdministrator
 
-  # Set the Window Title
+  # Set the window title
   Set-WindowTitle
+
+  # Get a random quote from Wikipedia
+  Get-Random "Genius", "Love", "Success", "Failure", "Intelligence" | Get-Quote
 
   # Import all my modules
   $my_modules = @('posh-git', 'oh-my-posh', 'Get-ChildItemColor')
